a12b8de01f024187da3d9d5d5606ee670b6d968b
Michael Kukar <mkukar@gmail.com>
2020-04-02 23:00:12 -0700

    merged all dicts into single one

--- webnavigator.py
+++ webnavigator.py
@@ -9,34 +9,22 @@ from selenium.webdriver.firefox.options import Options
 
 class WebNavigator:
 
-    SUPPORTED_SITES = ['PRIMENOW_AMAZON', 'PRIMENOW_WHOLE_FOODS']
-
-    LOGIN_URL_DICT = {
-        'PRIMENOW_AMAZON' : "https://primenow.amazon.com/signin?returnUrl=https://primenow.amazon.com/home",
-        'PRIMENOW_WHOLE_FOODS' : "https://primenow.amazon.com/signin?returnUrl=https://primenow.amazon.com/home"
-
-    }
-
-    LOGIN_FIELD_DICT = {
+    SITE_INFO_DICT = {
         'PRIMENOW_AMAZON' : {
-            'user' : 'ap_email',
-            'pass' : 'ap_password',
-            'submit' : 'signInSubmit'
-        },
-        'PRIMENOW_WHOLE_FOODS' : {
-            'user' : 'ap_email',
-            'pass' : 'ap_password',
-            'submit' : 'signInSubmit'
-        }
-    }
-
-    CART_DICT = {
-        'PRIMENOW_AMAZON' : {
+            'login_link' : 'https://primenow.amazon.com/signin?returnUrl=https://primenow.amazon.com/home',
+            'user_id' : 'ap_email',
+            'pass_id' : 'ap_password',
+            'submit_id' : 'signInSubmit',
             'cart_link' : 'https://primenow.amazon.com/checkout/enter-checkout?merchantId=AQCUSYIS91P2B',
             'delivery_options_id' : 'delivery-slot-form',
             'no_delivery_text' : 'No delivery windows available.'
+
         },
         'PRIMENOW_WHOLE_FOODS' : {
+            'login_link' : 'https://primenow.amazon.com/signin?returnUrl=https://primenow.amazon.com/home',
+            'user_id' : 'ap_email',
+            'pass_id' : 'ap_password',
+            'submit_id' : 'signInSubmit',
             'cart_link' : 'https://primenow.amazon.com/checkout/enter-checkout?merchantId=A292AGRFD023A8',
             'delivery_options_id' : 'delivery-slot-form',
             'no_delivery_text' : 'No delivery windows available.'
@@ -51,20 +39,20 @@ class WebNavigator:
 
 
     def login(self, site_id, browser, username, password, time_to_manually_retry=0):
-        if site_id not in self.SUPPORTED_SITES:
+        if site_id not in self.SITE_INFO_DICT.keys():
             return False
         # loads the login website
-        browser.get(self.LOGIN_URL_DICT[site_id])
+        browser.get(self.SITE_INFO_DICT[site_id]['login_link'])
         loginTitle = browser.title
 
         # enters credentials into the login fields
         time.sleep(3) # these delays are to make amazon think we are human
-        browser.find_element_by_id(self.LOGIN_FIELD_DICT[site_id]['user']).send_keys(username)
+        browser.find_element_by_id(self.SITE_INFO_DICT[site_id]['user_id']).send_keys(username)
         time.sleep(3)
-        browser.find_element_by_id (self.LOGIN_FIELD_DICT[site_id]['pass']).send_keys(password)
+        browser.find_element_by_id (self.SITE_INFO_DICT[site_id]['pass_id']).send_keys(password)
         time.sleep(3)
         # clicks the enter button
-        browser.find_element_by_id(self.LOGIN_FIELD_DICT[site_id]['submit']).click()
+        browser.find_element_by_id(self.SITE_INFO_DICT[site_id]['submit_id']).click()
 
         # if title stays the same as sign-in, then error occured (didn't redirect to main page)
         if browser.title == loginTitle:
@@ -80,20 +68,20 @@ class WebNavigator:
 
     # return tuple of True/False if delivery time found, and then the string of the time(s)
     def checkIfDeliveryTimeAvailable(self, site_id, browser):
-        if site_id not in self.SUPPORTED_SITES:
+        if site_id not in self.SITE_INFO_DICT.keys():
             return (False, "ERROR - unsupported site")
         
         # assumes you are already logged in
-        browser.get(self.CART_DICT[site_id]['cart_link'])
+        browser.get(self.SITE_INFO_DICT[site_id]['cart_link'])
         delivery_slots = None
         try:
             delivery_slots = browser.find_element_by_id(
-                self.CART_DICT[site_id]['delivery_options_id']
+                self.SITE_INFO_DICT[site_id]['delivery_options_id']
             )
         except Exception as e:
             return (False, "ERROR - Could not find delivery option")
 
-        if self.CART_DICT[site_id]['no_delivery_text'] in delivery_slots.text:
+        if self.SITE_INFO_DICT[site_id]['no_delivery_text'] in delivery_slots.text:
             return (False, "No slots")
         else:
             return (True, delivery_slots.text)
